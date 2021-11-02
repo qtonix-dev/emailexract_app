1cfa3a95ddfde7d8b264dc516e914e06394d7815
Biswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-11-02 08:32:11 +0530

    '02NOV2021

--- src/pages/bulks/domain/BulkDomainCreateNew.js
+++ src/pages/bulks/domain/BulkDomainCreateNew.js
@@ -166,7 +166,7 @@ export class BulkDomainCreate extends Component {
 
     fetchRecord(domain){
         this.setState({currentextractdomainname:domain})
-        axios.get(`https://emailextractserver2bulkgetinfo.herokuapp.com/api/bulkdomainextract/testdomainextrat/${domain}`,{timeout:15000})
+        axios.get(`https://emailextractserver2bulkgetinfo.herokuapp.com/api/bulkdomainextract/testdomainextrat/${domain}`,{timeout:5000})
         .then(response=>{
             var bulkdomainextratdata = this.state.bulkdomainextratdata;
             var msdata= response.data;
